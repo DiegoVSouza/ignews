304af4255ef1960881b697693ab8ca9e89238aa4
Diego Souza <dvsdiegovieira@gmail.com>
2022-09-30 07:13:36 -0300

    env

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,28 +1,28 @@
-import { FaGithub} from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 import styles from './styles.module.scss'
-import {FiX} from 'react-icons/fi'
+import { FiX } from 'react-icons/fi'
 import { signIn, signOut, useSession } from 'next-auth/react'
 
-export function SingInButton(){
+export function SingInButton() {
 
-    const {data} = useSession()
+    const { data } = useSession()
 
 
     return data ? (
-        <button 
-        className={styles.singInButton}
-        onClick={()=> signOut()}>
-        <FaGithub color='#04d361' />
-        {data.user.name}
-        <FiX color='#737380' className={styles.closeIcon}/>
-    </button>
+        <button
+            className={styles.singInButton}
+            onClick={() => signOut()}>
+            <FaGithub color='#04d361' />
+            {data.user.name}
+            <FiX color='#737380' className={styles.closeIcon} />
+        </button>
     ) : (
-        <button 
-        className={styles.singInButton}
-        onClick={()=>signIn('github')}
+        <button
+            className={styles.singInButton}
+            onClick={() => signIn('github')}
         >
-        <FaGithub color='#eba417' />
-        Sing in with GiHub
-    </button>
+            <FaGithub color='#eba417' />
+            Sing in with GitHub
+        </button>
     );
 }
